22eda431da7f48707cdcb9d4826fc24622ebe7e2
phduyanhhh <aphan1304@gmail.com>
2025-12-07 23:48:40 +0700

    feat(Employee): get list employyees

---
+++ src/G7.Foss.Application/Services/Employees/Dtos/EmployeeListDto.cs
@@ -0,0 +1,18 @@
+using System;
+using G7.Foss.Entities;
+
+namespace G7.Foss.Services.Employees.Dtos;
+
+public class EmployeeListDto
+{
+    public int Id {get; set;}
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Address { get; set; }
+    public double Salary { get; set; }
+    public PositionEnum Position { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public GenderEnum Gender { get; set; }
+}
--- src/G7.Foss.Application/Services/Employees/EmployeeAppServices.cs
+++ src/G7.Foss.Application/Services/Employees/EmployeeAppServices.cs
@@ -1,7 +1,14 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Dynamic.Core;
 using System.Threading.Tasks;
+using Abp.Application.Services.Dto;
 using Abp.Domain.Repositories;
+using Abp.Extensions;
+using Abp.Linq.Extensions;
 using G7.Foss.Entities;
 using G7.Foss.Services.Employees.Dtos;
+using Microsoft.EntityFrameworkCore;
 
 namespace G7.Foss.Services.Employees;
 
@@ -30,5 +37,37 @@ public class EmployeeAppServices : FossAppServiceBase, IEmployeeAppServices
         };
         
         await _repositoryEmployeeAppService.InsertAsync(newEmployee);
+    }
+
+    public async Task<PagedResultDto<EmployeeListDto>> GetList(GetAllEmployeeInput input)
+    {
+        IQueryable<Employee> query = _repositoryEmployeeAppService
+            .GetAll()
+            .WhereIf(!input.Search.IsNullOrWhiteSpace(),
+                x => x.FirstName.ToLower().Contains(input.Search.ToLower()));
+
+        int totalCount = await query.CountAsync();
+
+        if (input.Sorting.IsNullOrWhiteSpace())
+        {
+            input.Sorting = "CreationTime DESC";
+        }
+
+        List<EmployeeListDto> items = await query.PageBy(input).OrderBy(input.Sorting)
+            .Select(x => new EmployeeListDto
+            {
+                Id = x.Id,
+                FirstName = x.FirstName,
+                LastName = x.LastName,
+                Email = x.Email,
+                PhoneNumber = x.PhoneNumber,
+                Address = x.Address,
+                Salary = x.Salary,
+                Position = x.Position,
+                DateOfBirth = x.DateOfBirth,
+                Gender = x.Gender
+            }).ToListAsync();
+
+        return new PagedResultDto<EmployeeListDto>(totalCount, items);
     }
 }
